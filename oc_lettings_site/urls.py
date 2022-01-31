d5e61d6cfabb3eecc80ea190e854e1c1a3c4d3e4
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-01-31 18:11:06 +0100

    urls conf updated + namespace added for profiles app + urls in templates updated

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from lettings.views import index, letting
-from profiles.views import profile, profiles_index
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace='lettings')),
-    path('profiles/', profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', profile, name='profile'),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
 ]
