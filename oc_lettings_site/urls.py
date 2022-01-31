e0947bffb0bc6b42c38c98f5578aaeb29d15e080
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-01-31 16:45:19 +0100

    Creation of new app called profiles and transfer of data in sqlite3 tables

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 from lettings.views import letting, lettings_index
+from profiles.views import profile, profiles_index
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
