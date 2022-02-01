7ee027ca478d775890d301a7bfbc6a61e5e83352
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-02-01 14:59:38 +0100

    Tests for letings added

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,62 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from .models import Address, Letting
+
 
 # Create your tests here.
+class IndexTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse('lettings:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(reverse('lettings:index'))
+        self.assertTemplateUsed(response, 'lettings/index.html')
+
+    def test_title(self):
+        response = self.client.get(reverse('lettings:index'))
+        self.assertContains(response,"<title>Lettings</title>")
+
+
+class IndexNoLettingTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse('lettings:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(reverse('lettings:index'))
+        self.assertTemplateUsed(response, 'lettings/index.html')
+
+    def test_title(self):
+        response = self.client.get(reverse('lettings:index'))
+        self.assertContains(response,"<p>No lettings are available.</p>")
+
+
+class LettingTest(TestCase):
+    letting_title = "Nantes"
+
+    @classmethod
+    def setUpTestData(cls):
+        # Creates an address and a letting for the tests
+        address = Address.objects.create(number=1,
+                                         zip_code=44000)
+        cls.letting = Letting.objects.create(title=cls.letting_title,
+                                             address=address)
+
+    def test_profile_status_code(self):
+        response = self.client.get(reverse('lettings:letting',
+                                           kwargs={"letting_id":1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_template_used(self):
+        response = self.client.get(reverse('lettings:letting',
+                                           kwargs={"letting_id":1}))
+        self.assertTemplateUsed(response, 'lettings/letting.html')
+
+    def test_profile_title(self):
+        response = self.client.get(reverse('lettings:letting',
+                                           kwargs={"letting_id":1}))
+        self.assertContains(response,
+                            "<title>"+LettingTest.letting_title+"</title>")
