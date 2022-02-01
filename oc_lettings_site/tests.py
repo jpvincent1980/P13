f23ea2e6d567405655f18f67dc8e168a8e94f6ce
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-02-01 14:18:56 +0100

    Tests for index page added

---
+++ oc_lettings_site/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+# Create your tests here.
+class IndexTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_title(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response,"<title>Holiday Homes</title>")
