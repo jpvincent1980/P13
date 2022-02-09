f3a44094b525eb7d3c55df25398e344acf08a30a
jpvincent1980 <55929639+jpvincent1980@users.noreply.github.com>
2022-02-09 16:02:36 +0100

    Update views.py

--- lettings/views.py
+++ lettings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .models import Letting
 
-
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 # Create your views here.
 # Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit.
 # Sed non placerat massa. Integer est nunc, pulvinar a tempor et,
